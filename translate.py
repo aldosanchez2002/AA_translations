e0c6b372411308246faa64f545b76bac8eecc340
EdwinTomy <57510528+EdwinTomy@users.noreply.github.com>
2023-11-13 19:28:38 -0700

    memoitization

--- translate.py
+++ translate.py
@@ -35,11 +35,11 @@ translators = { ("alaw", "ulaw") : 1,
                 ("slin", "gsm") : 6
                }
 
-def translationPath(in_codec, out_codec):
+def translationPath(in_codec, out_codec, graph):
     if in_codec == out_codec:
         return [], 0
-    graph = edgesToGraph(translators)
     path, cost = dijkstraMST(graph, in_codec, out_codec)
+    #print(graph)
     return path, cost
 
 def edgesToGraph(edges_dict):
@@ -48,7 +48,7 @@ def edgesToGraph(edges_dict):
         source, dest = source_dest
         if source not in graph:
             graph[source] = {}
-        graph[source][dest] = cost
+        graph[source][dest] = (cost, [source, dest])
     return graph
 
 def dijkstraMST(graph, start, end):
@@ -63,8 +63,18 @@ def dijkstraMST(graph, start, end):
     while end not in visited:
         visited.add(node)
         MST[node] = prev
+        path = get_path(MST, start, end)
+        if graph[start] == None:
+          graph[start][end] = (min_cost[end], path)
+        elif end not in graph[start]:
+          graph[start][end] = (min_cost[end], path)
+        elif graph[start][end][0] >min_cost[end]:
+          graph[start][end] = (min_cost[end], path)
+
+
         prev = node
-        for neighbor, cost in graph[node].items():
+        for neighbor, cost_path in graph[node].items():
+            cost, path = cost_path
             min_cost[neighbor] = min(min_cost[node] + cost, min_cost[neighbor])
         node = choose_node(min_cost, visited)
         if not node:
@@ -74,8 +84,9 @@ def dijkstraMST(graph, start, end):
     # print(min_cost)
     # print(MST[start],MST[end])
     # print(min_cost[start],min_cost[end])
-    #print(MST)
+
     path = get_path(MST, start, end)
+    graph[start][end] = (min_cost[end], path)
     return path, min_cost[end]
 
 def get_path(MST, start, end):
@@ -104,24 +115,24 @@ def choose_node(min_cost, visited):
             min_cost_value = cost
     return min_cost_node
 
-def test1():
+def test0():
     langs = ['alaw', 'ulaw', 'slin', 'g722', 'slin16', 'g729', 'g7222', 'g723', 'clearmode', 'lpc10', 'ilbc', 'speex', 'speex16', 'g726', 'gsm']
-    ans1 = []
+    ans0 = []
     for lang1 in langs:
         for lang2 in langs:
             if lang1 != lang2:
-                path, cost = translationPath(lang1, lang2)
+                graph = edgesToGraph(translators)
+                path, cost = translationPath(lang1, lang2, graph)
                 if cost == float('inf'):
                     print("No translation path was found from {} to {}.".format(lang1, lang2))
+                    ans0.append("NO")
                 else:
                     print("From {} to {}, \n\tPath: {} \n\tMinCost {}.".format(lang1, lang2, "->".join(path), cost))
-                    ans1.append(cost)
-                    #pass
-
-    return ans1
+                    ans0.append(cost)
+    return ans0
 
 if __name__ == '__main__':
-    ans1 = test1()
+    ans0 = test0()
     # if len(sys.argv) < 3:
     #     print("Not enough arguments. Give input and output codec.")
     #     sys.exit(1)
